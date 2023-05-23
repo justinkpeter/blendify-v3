f2c76523bcc98d91b1f39c424be233239ba64dc6
Justin <justinkmpeter@gmail.com>
2023-05-23 10:44:17 -0500

    Adding commas to render in lists

--- components/CarouselItem.tsx
+++ components/CarouselItem.tsx
@@ -3,18 +3,9 @@ import React from "react";
 export const CarouselItem = ({item}:any) => {
     return (
         <>
-            {/*<div className="carousel-item h-fit relative">*/}
-            {/*    <div className={'w-full h-full relative flex flex-col'}>*/}
-            {/*        <img src="https://i.scdn.co/image/ab6761610000e5ebb6124dc2f1c2d9ae143d36ab" className="object-cover object-center drop-shadow-2xl max-w-[150px] max-h-[150px]" />*/}
-            {/*        hello*/}
-
-            {/*    </div>*/}
-            {/*</div>*/}
-
             <div className={'w-fit h-fit relative p-3 rounded-lg backdrop-blur-xl bg-white/10 hover:bg-white/30 transition-all duration-100 ease-in-out group'}>
                 <div className={'flex flex-col space-y-4 relative'}>
                     <div className={'relative h-[150px] w-[150px] overflow-hidden'}>
-                        {/*<img src="https://i.scdn.co/image/ab6761610000e5ebb6124dc2f1c2d9ae143d36ab"*/}
                         <img src={item && item?.album?.images[0]?.url || item?.images[0]?.url}
                              alt={item?.name}
                              className="absolute inset-0 object-cover w-full h-full object-center drop-shadow-3xl group-hover:scale-125 transition-all duration-500 ease-in-out"/>
@@ -23,20 +14,21 @@ export const CarouselItem = ({item}:any) => {
                         <div> {item?.name} </div>
                         <Image src={'/assets/logo/Spotify_Icon_RGB_White.png'} alt={'Spotify'} width={30} height={30}/>
                     </div>
-                    {/*{ render artists  }*/}
-
                     <div className={'flex flex-wrap gap-1'}>
-                        { item?.artists?.map((artist:any) => {
+                        {item?.artists?.map((artist: any, index: number) => {
+                            const isLastItem = index === item.artists.length - 1;
+                            const comma = isLastItem ? '' : ', ';
                             return (
-                                <div className={'text-white/60'} key={artist.id}> {artist.name} </div>
-                            )
-                        }) }
+                                <div className={'text-white/60'} key={artist.id}>
+                                    {artist.name}
+                                    {comma}
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
                 <a className={'h-full w-full absolute top-0 preview__uri'} href={item?.uri}></a>
-                    {/*<div className={'absolute bottom-4 right-4  flex items-center justify-center'}>*/}
-                    {/*    <Image src={'/assets/logo/Spotify_Icon_RGB_White.png'} alt={'Spotify'} width={30} height={30}/>*/}
-                    {/*</div>*/}
+
             </div>
         </>
     );
