e276bc562f4b36bec403e5ed93bb7cd2a22ed086
Justin <justinkmpeter@gmail.com>
2023-05-23 11:13:03 -0500

    Increased footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import { ChevronUpDownIcon } from "@heroicons/react/24/solid";
 export const Footer = () => {
     return (
         <>
-            <footer className={'fixed bottom-2 pl-2  h-fit flex gap 1.5 items-center text-white/30 text-sm uppercase '}>
-                <ChevronUpDownIcon className={'h-5 w-5 '}/> Scroll to navigate
+            <footer className={'fixed bottom-2 pl-2  h-fit flex gap 1.5 items-center text-white text-sm mix-blend-difference uppercase '}>
+                <ChevronUpDownIcon className={'h-8 w-8 '}/> Scroll to navigate
             </footer>
         </>
     );
