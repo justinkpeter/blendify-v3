229277fd8145ed7a605f14008e8a733eccef3518
Justin <justinkmpeter@gmail.com>
2024-12-11 18:34:03 -0600

    convert lib to ts

---
+++ lib/spotify.ts
@@ -0,0 +1,23 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const scopes = ["user-top-read"].join(",");
+
+const params = {
+  scope: scopes,
+  client_id: process.env.SPOTIFY_CLIENT_ID!,
+  response_type: "code",
+  redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/spotify`,
+  show_dialog: "true",
+};
+
+const queryParamString = new URLSearchParams(params).toString();
+const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParamString}`;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  redirectUri: `${process.env.NEXTAUTH_URL}/api/auth/callback/spotify`,
+});
+
+export default spotifyApi;
+export { LOGIN_URL };
